6bffac09d2fd33ce4d04598179fe8371ca03882b
Renuka <renukakushwah60@gmail.com>
2026-06-10 16:08:24 +0530

    coordinate

--- frontend/src/Socket.js
+++ frontend/src/Socket.js
@@ -1,6 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://womensafety-r0s4.onrender.com");
-// const socket = io("http://localhost:8080");
+const socket = io(
+  "https://womensafety-r0s4.onrender.com",
+  {
+    transports: ["websocket", "polling"],
+    reconnection: true
+  }
+);
 
 export default socket;
